5d4bd44993a3b8f1a6a55a3a8dca62a9fcf208a8
fatmaayachi <fatma.ayechi@hotmail.com>
2019-03-27 22:02:07 +0100

    jeu8

--- JS/jeu.js
+++ JS/jeu.js
@@ -1,25 +1,24 @@
-var a= Math.floor(Math.random() * 100) + 1;
-var nb=Number(prompt("saisir votre nombre"));
+var a=Math.floor(Math.random()*100+1);
+var nombre;
 var i=0;
 var test=1;
 while(i<8 && test==1)
 {
+    nombre=Number(prompt("saisir votre nombre"));
     
-    if(nb==a)
+    if(nombre==a)
     {
         alert("bravo!vous avez gagner");
         test=0;
     }
-    if(nb<a)
+    else if(nombre<a)
     {
-        alert("le nombre est plus petit");
+        alert("le nombre est plus petit,Retente ta chance !");
         i++;
-        nombre = Number(prompt("Retente ta chance !"));
     }
-    if(nb>a)
+    else if(nombre>a)
     {
-        alert("le nombre est plus grand");
+        alert("le nombre est plus grand,Retente ta chance !");
         i++;
-        nombre = Number(prompt("Retente ta chance !"));
     }
 }
