466c3d8c1de0b4b4a5e32d36f335b9f17ebb2da0
fatmaayachi <fatma.ayechi@hotmail.com>
2019-03-27 21:34:07 +0100

    jeu2

--- JS/jeu.js
+++ JS/jeu.js
@@ -1,4 +1,4 @@
-var a=Math.floor(Math.random()*100+1);
+var a= Math.floor(Math.random() * 100) + 1;
 var nombre=Number(prompt("saisir votre nombre"));
 var i=0;
 var test=1;
@@ -10,13 +10,13 @@ while(i<8 && test==1)
         alert("bravo!vous avez gagner");
         test=0;
     }
-    else if(nb<a)
+    if(nb<a)
     {
         alert("le nombre est plus petit");
         i++;
         nombre = Number(prompt("Retente ta chance !"));
     }
-    else if(nb>a)
+    if(nb>a)
     {
         alert("le nombre est plus grand");
         i++;
