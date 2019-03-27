85ca71896f0918772e6c571cd272f67e630c0e53
fatmaayachi <fatma.ayechi@hotmail.com>
2019-03-27 21:24:53 +0100

    jeu1

--- JS/jeu.js
+++ JS/jeu.js
@@ -1,22 +1,25 @@
 var a=Math.floor(Math.random()*100+1);
+var nombre=Number(prompt("saisir votre nombre"));
 var i=0;
 var test=1;
 while(i<8 && test==1)
 {
-    var nb=number(prompt('saisir votre nombre'));
+    
     if(nb==a)
     {
-        alert('bravo!vous avez gagner');
+        alert("bravo!vous avez gagner");
         test=0;
     }
     else if(nb<a)
     {
-        alert('le nombre est plus petit');
+        alert("le nombre est plus petit");
         i++;
+        nombre = Number(prompt("Retente ta chance !"));
     }
     else if(nb>a)
     {
-        alert('le nombre est plus grand');
+        alert("le nombre est plus grand");
         i++;
+        nombre = Number(prompt("Retente ta chance !"));
     }
 }
